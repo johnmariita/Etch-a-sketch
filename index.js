42dfac30c124cd633a4a2a4bd10503d2866d7245
johnmariita <johnmariita421@gmail.com>
2024-10-08 20:05:17 +0300

    added the drawing and erasing functionalities

--- index.js
+++ index.js
@@ -1,25 +1,38 @@
+let grids = 16;
+let currentTool = "pencil"; //will be switching to eraser when eraser is clicked
 const pad = document.querySelector('.container .drawingPad');
-const btns = document.getElementsByClassName('btn');
-console.log(btns);
-for (let i = 0; i < btns.length; i++) {
-    btns[i].addEventListener('click', () => {
-        btns[i].classList.toggle("toggle");
-    });
-}
+const penBtn = document.querySelector('.container .tools .pencil');
+const erBtn = document.querySelector('.container .tools .eraser');
+const clear = document.querySelector('.container .tools .clear');
+
+clear.addEventListener('click', () => {
+    while (pad.firstChild) {
+        pad.removeChild(pad.lastChild);
+    }
+    createGrid(grids);
+})
+penBtn.addEventListener('click', () => {
+    if (erBtn.classList.contains('toggle')) erBtn.classList.toggle('toggle');
+    penBtn.classList.toggle('toggle');
+    currentTool = "pencil";
+});
+erBtn.addEventListener('click', () => {
+    if (penBtn.classList.contains('toggle')) penBtn.classList.toggle('toggle');
+    erBtn.classList.toggle('toggle');
+    currentTool = "eraser";
+});
 function createGrid(gridSize) {
     for(let i = 0; i < gridSize; i++) {
             for (let j = 0; j < gridSize; j++) {
             const pixel = document.createElement('div');
             pixel.style.width = 560 / gridSize + "px";
             pixel.style.height = 560 / gridSize + "px";
-            if ((j % 2 === 0 && i % 2 === 0) || (j % 2 !== 0 && i % 2 !== 0)) {
-                pixel.style.backgroundColor = "rgb(140, 140, 140)";
-                console.log(pixel.style.backgroundColor, i, j);
-            } else {
-                pixel.backgroundColor = "rgb(255. 255, 255)";
-                console.log(pixel.style.backgroundColor, i, j);
-            }
-            
+            pixel.backgroundColor = "rgb(255. 255, 255)";
+            pixel.addEventListener('mouseover', () => {
+                if (currentTool === 'pencil') {
+                    pixel.style.backgroundColor = 'black';
+                } else pixel.style.backgroundColor = 'white';
+            });
             pad.appendChild(pixel);
         }
     }
@@ -28,13 +41,13 @@ function createGrid(gridSize) {
 
 
 function requestGridSize () {
-    let size = parseInt(prompt("What size do you want? Range: 16 X 16 - 100 X 100"));
-    if (size > 100) size = 100;
-    if (size < 16) size = 16;
+    grids = parseInt(prompt("What size do you want? Range: 16 X 16 - 100 X 100"));
+    if (grids > 100) grids = 100;
+    if (grids < 16) grids = 16;
     while (pad.firstChild) {
         pad.removeChild(pad.lastChild);
     }
-    createGrid(size);
+    createGrid(grids);
 }
 
-createGrid(16);
+createGrid(grids);
