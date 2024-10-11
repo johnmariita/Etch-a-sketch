fc5473db7a7495905eb8222e1b68b9f883bbd96e
johnmariita <johnmariita421@gmail.com>
2024-10-11 22:05:20 +0300

    only draws whn the mouse is held down, added the random color feature.

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 let grids = 16;
+let red;
+let green;
+let blue;
+let isDrawing = false;
+
 let currentTool = "pencil"; //will be switching to eraser when eraser is clicked
 const pad = document.querySelector('.container .drawingPad');
 const penBtn = document.querySelector('.container .tools .pencil');
 const erBtn = document.querySelector('.container .tools .eraser');
+const colored = document.querySelector('.container .tools .colors')
 const clear = document.querySelector('.container .tools .clear');
 
 clear.addEventListener('click', () => {
@@ -13,25 +19,48 @@ clear.addEventListener('click', () => {
 })
 penBtn.addEventListener('click', () => {
     if (erBtn.classList.contains('toggle')) erBtn.classList.toggle('toggle');
+    if (colored.classList.contains('toggle')) colored.classList.toggle('toggle');
     penBtn.classList.toggle('toggle');
     currentTool = "pencil";
 });
+colored.addEventListener('click', () => {
+    if (penBtn.classList.contains('toggle')) penBtn.classList.toggle('toggle');
+    if (erBtn.classList.contains('toggle')) erBtn.classList.toggle('toggle');
+    colored.classList.toggle('toggle');
+    if (currentTool === "paint") currentTool = "pencil";
+    else currentTool = "paint";
+})
 erBtn.addEventListener('click', () => {
+    if (colored.classList.contains('toggle')) colored.classList.toggle('toggle');
     if (penBtn.classList.contains('toggle')) penBtn.classList.toggle('toggle');
     erBtn.classList.toggle('toggle');
-    currentTool = "eraser";
+    if (currentTool === "eraser") currentTool = "pencil";
+    else currentTool = "eraser";
 });
 function createGrid(gridSize) {
     for(let i = 0; i < gridSize; i++) {
-            for (let j = 0; j < gridSize; j++) {
+        for (let j = 0; j < gridSize; j++) {
             const pixel = document.createElement('div');
             pixel.style.width = 560 / gridSize + "px";
             pixel.style.height = 560 / gridSize + "px";
-            pixel.backgroundColor = "rgb(255. 255, 255)";
+            pixel.addEventListener('mousedown', () => {
+                isDrawing = true;
+            });
+            pixel.addEventListener('mouseup', () => {
+                isDrawing = false;
+            });
             pixel.addEventListener('mouseover', () => {
-                if (currentTool === 'pencil') {
-                    pixel.style.backgroundColor = 'black';
-                } else pixel.style.backgroundColor = 'white';
+                if (isDrawing) {
+                    if (currentTool === 'pencil') {
+                        pixel.style.backgroundColor = 'rgb(0, 0, 0)';
+                    } else if (currentTool === 'eraser') pixel.style.backgroundColor = 'white';
+                    else {
+                        red = Math.floor(Math.random() * 256);
+                        green = Math.floor(Math.random() * 256);
+                        blue = Math.floor(Math.random() * 256);
+                        pixel.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+                    }
+                }
             });
             pad.appendChild(pixel);
         }
